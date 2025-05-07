b869d9541e34d0872999f53298933d8ef56a451c
Nikola Valkov <bulgarianmapper64@gmail.com>
2025-05-07 19:08:49 +0300

    removed no var warning

--- nextjs-blog/eslint.config.mjs
+++ nextjs-blog/eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    files: ["src/lib/prisma/index.ts"],
+    rules: {
+      "no-var": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
--- nextjs-blog/src/lib/prisma/index.ts
+++ nextjs-blog/src/lib/prisma/index.ts
@@ -1,12 +1,12 @@
-// src/lib/prisma/index.ts
 import { PrismaClient } from "@prisma/client";
 
+// eslint-disable-next-line no-var
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma || new PrismaClient();
+export const prisma = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
+  globalThis.prisma = prisma;
 }
